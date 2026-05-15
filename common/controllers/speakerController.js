becd771db5f348af02bb1e466c905703e6be21ce
PedroCopette <pedrolixinski2005@gmail.com>
2026-05-15 13:09:22 -0300

    fix: corrige rotas e controllers de palestrantes e noticias

--- common/controllers/speakerController.js
+++ common/controllers/speakerController.js
@@ -1,57 +1,102 @@
 const Speaker = require("../models/Speaker");
 
-exports.createSpeaker = async (req, res) => {
+const createSpeaker = async (req, res) => {
+
   try {
+
     const speaker = await Speaker.create(req.body);
 
     res.status(201).json(speaker);
+
   } catch (error) {
+
     res.status(500).json({
-      error: error.message,
+      error: error.message
     });
+
   }
+
 };
 
-exports.getSpeakers = async (req, res) => {
+const getSpeakers = async (req, res) => {
+
   try {
-    const speakers = await Speaker.find();
+
+    const speakers = await Speaker.findAll();
 
     res.json(speakers);
+
   } catch (error) {
+
     res.status(500).json({
-      error: error.message,
+      error: error.message
     });
+
   }
+
 };
 
-exports.updateSpeaker = async (req, res) => {
+const updateSpeaker = async (req, res) => {
+
   try {
-    const speaker = await Speaker.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      {
-        new: true,
-      }
-    );
+
+    const speaker = await Speaker.findByPk(req.params.id);
+
+    if (!speaker) {
+
+      return res.status(404).json({
+        error: "Palestrante não encontrado"
+      });
+
+    }
+
+    await speaker.update(req.body);
 
     res.json(speaker);
+
   } catch (error) {
+
     res.status(500).json({
-      error: error.message,
+      error: error.message
     });
+
   }
+
 };
 
-exports.deleteSpeaker = async (req, res) => {
+const deleteSpeaker = async (req, res) => {
+
   try {
-    await Speaker.findByIdAndDelete(req.params.id);
+
+    const speaker = await Speaker.findByPk(req.params.id);
+
+    if (!speaker) {
+
+      return res.status(404).json({
+        error: "Palestrante não encontrado"
+      });
+
+    }
+
+    await speaker.destroy();
 
     res.json({
-      message: "Palestrante removido",
+      message: "Palestrante removido"
     });
+
   } catch (error) {
+
     res.status(500).json({
-      error: error.message,
+      error: error.message
     });
+
   }
+
+};
+
+module.exports = {
+  createSpeaker,
+  getSpeakers,
+  updateSpeaker,
+  deleteSpeaker
 };
