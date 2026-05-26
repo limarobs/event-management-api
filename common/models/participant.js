6c891cf6b4934bf95a76be2b5ec741738e2d4351
lucas-delazeri <lucasdelazeris@gmail.com>
2026-05-26 11:58:02 -0300

    fix: set default value on approvalStatus from 'approved' to 'pending'

--- common/models/participant.js
+++ common/models/participant.js
@@ -26,7 +26,7 @@ const Participant = sequelize.define('Participant', {
     approvalStatus: {
         type: DataTypes.ENUM('pending', 'approved', 'rejected'),
         allowNull: false,
-        defaultValue: 'approved',
+        defaultValue: 'pending',
         field: 'status'
     },
 
